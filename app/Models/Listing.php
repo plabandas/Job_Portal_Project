c409b65ea42032bff017802a2344f413fb0053aa
Plaban <turjoyplaban09@gmail.com>
2024-04-14 10:30:30 +0600

    Listing deleted and  create a new model from command

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,40 +2,10 @@
 
 namespace App\Models;
 
-class Listing {
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
- public static function all(){
-    return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae tempore porro, nihil molestiae ipsum fugiat eveniet aspernatur doloremque minima ut. Dignissimos dolore unde nisi quae, dolorem quidem nesciunt necessitatibus voluptas.',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae tempore porro, nihil molestiae ipsum fugiat eveniet aspernatur doloremque minima ut. Dignissimos dolore unde nisi quae, dolorem quidem nesciunt necessitatibus voluptas.',
-            ]
-    ];
- }
-
- public static function find($id){
-
-    $listings = self::all();
-
-    foreach ($listings as $listing){
-        if($listing['id'] == $id){
-            return $listing;
-        }
-    }
-
- }
-
-
-
-
+class Listing extends Model
+{
+    use HasFactory;
 }
-
-
-
-?>
