1e113c3e6c55402b22e0072af95774dddf010866
Plaban <turjoyplaban09@gmail.com>
2024-04-15 22:07:05 +0600

    Logout success with show user name

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -29,6 +29,24 @@
     <nav class="flex justify-between items-center mb-4">
         <a href="/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
+
+            @auth
+            <li>
+                <span class="font-bold uppercase">{{auth()->user()->name}}</span>
+            </li>
+            <li>
+                <a href="/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>
+                    Manage Listings</a>
+            </li>
+            <li>
+               <form class="inline" method="POST" action="/logout">
+               @csrf
+               <button type="submit">
+                <i class="fa-solid fa-door-closed"></i> Logout
+               </button>
+               </form>
+            </li>
+            @else
             <li>
                 <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
             </li>
@@ -36,6 +54,7 @@
                 <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>
                     Login</a>
             </li>
+            @endauth
         </ul>
     </nav>
 
