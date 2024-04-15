02eef90fd7c6c99ba8c20648983e7ebea077051e
Plaban <turjoyplaban09@gmail.com>
2024-04-16 00:21:33 +0600

    Listing and Users Connections

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\User;
 use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
@@ -18,9 +19,17 @@ class DatabaseSeeder extends Seeder
     {
 
         //This is for dummy 10
-         \App\Models\User::factory(10)->create();
+        //  \App\Models\User::factory(10)->create();
 
-         Listing::factory(6)->create();
+        $user = User::factory()->create([
+             'name' => 'John Doe',
+             'email'=> 'john@gmail.com',
+             'password'=> bcrypt('123456')
+        ]);
+
+         Listing::factory(6)->create([
+            'user_id' => $user->id
+         ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
